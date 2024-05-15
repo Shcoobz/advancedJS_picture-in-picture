601e8804a958aecdd716ab1be2bd731cc8e079ed
Shcoobz <a.boeckmann23@gmail.com>
2024-05-15 10:50:16 +0200

    implemented functionality for selecting screen & starting picture in picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,66 @@
+import { useState, useRef } from 'react';
 
 function App() {
+  const [mediaStream, setMediaStream] = useState(null);
+  const [showStartPipButton, setShowStartPipButton] = useState(false);
+  const [isPipActive, setIsPipActive] = useState(false);
+  const videoElement = useRef(null);
+
+  async function selectMediaStream() {
+    try {
+      const stream = await navigator.mediaDevices.getDisplayMedia();
+      setMediaStream(stream);
+      videoElement.current.srcObject = stream;
+      setShowStartPipButton(true);
+    } catch (error) {
+      console.error('Error selecting media stream:', error);
+    }
+  }
+
+  function handleStartPip() {
+    videoElement.current.play().then(() => {
+      videoElement.current
+        .requestPictureInPicture()
+        .then(() => {
+          setIsPipActive(true);
+          setShowStartPipButton(false);
+        })
+        .catch((error) => {
+          console.error('Failed to start Picture in Picture', error);
+        });
+    });
+  }
+
+  function stopPictureInPicture() {
+    if (mediaStream) {
+      mediaStream.getTracks().forEach((track) => track.stop());
+      setIsPipActive(false);
+      setMediaStream(null);
+      setShowStartPipButton(false);
+      document.exitPictureInPicture().catch((error) => {
+        console.error('Error exiting Picture in Picture:', error);
+      });
+    }
+  }
+
   return (
-    <div className='App'>
-      <h1>Hello</h1>
+    <div className='app'>
+      {!showStartPipButton && !isPipActive && (
+        <div className='button-container'>
+          <button onClick={selectMediaStream}>Share Screen</button>
+        </div>
+      )}
+      {showStartPipButton && (
+        <div className='button-container'>
+          <button onClick={handleStartPip}>Start Picture in Picture</button>
+        </div>
+      )}
+      {isPipActive && (
+        <div className='button-container'>
+          <button onClick={stopPictureInPicture}>Stop</button>
+        </div>
+      )}
+      <video ref={videoElement} controls className='hiddenVideo'></video>
     </div>
   );
 }
